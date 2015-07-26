8b6e4ffac1c7d4648b9e37b5e9aa73ae111e4062
airys001 <airys001@gmail.com>
2015-07-27 02:57:43 +0800

    Signed-off-by: airys001 <airys001@gmail.com>

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix just create a matrix, and store its inverse.
+## test code: a <-makeCacheMatrix(matrix(c(3,2,4,5,-1,2,-1,3,-3),3,3))
+## test code: b<-cacheSolve(a)
+## test code: a$getm(), to get the matrix a
+## test code: a$getinv(), to get the stored inverse, if any. Should be NULL for the first time(or matrix changed)
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv_p <- NULL
+  setm <- function(y) {
+    x <<- y
+    inv_p <<- NULL
+  }
+  getm <- function() x
+  setinv <- function(inve) inv_p <<- inve
+  getinv <- function() inv_p
+  list(setm = setm, getm = getm,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## Cachesolve is to solve the inverse and store the value into makeChaeMatrix's inv_p
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inv_p<-x$getinv()
+  if(!is.null(inv_p)) {
+    message("getting cached inverse matrix")
+    return(inv_p)
+  }
+  data2 <- x$getm()
+  inv_p <- solve(data2, ...)
+  x$setinv(inv_p)
+  inv_p
 }
